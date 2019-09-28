a3842b40c730f94f9014898f94fdc20bc757c9a9
Alexey Merkulov <bgates@inbox.ru>
2019-09-28 12:02:23 +0300

    Добавлены первые 3 блока

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
+
+  // СЛАЙДЕРЫ
+
   $(".small-yellow-kitchen").slick({
     infinite: true,
     dots: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: '.item__right-arrow',
-    prevArrow: '.item__left-arrow'
+    nextArrow: '.small-yellow-kitchen~.item__right-arrow',
+    prevArrow: '.small-yellow-kitchen~.item__left-arrow'
   });
+
+  $(".small-white-kitchen").slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '.small-white-kitchen~.item__right-arrow',
+    prevArrow: '.small-white-kitchen~.item__left-arrow'
+  });
+
+  $(".small-purple-kitchen").slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '.small-purple-kitchen~.item__right-arrow',
+    prevArrow: '.small-purple-kitchen~.item__left-arrow'
+  });
+
 
 })
