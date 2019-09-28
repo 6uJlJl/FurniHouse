90ae5c0c8dbc81a4406f3bdab8655ec20e278d30
Alexey Merkulov <bgates@inbox.ru>
2019-09-28 11:08:42 +0300

    Верстка основной карточки без адаптива

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-
+  $(".small-yellow-kitchen").slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '.item__right-arrow',
+    prevArrow: '.item__left-arrow'
+  });
 
 })
