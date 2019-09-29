624d0c57d289870429c11c6873e6b3c62e143abc
Alexey Merkulov <bgates@inbox.ru>
2019-09-29 22:07:02 +0300

    Подключен fancybox, blazy для фотографий

--- js/main.js
+++ js/main.js
@@ -1,77 +1,22 @@
 $(document).ready(function(){
 
+  var bLazy = new Blazy({
+    selector : 'img',
+    offset: 300,
+  });
+
   // СЛАЙДЕРЫ
+  $(".fancybox").fancybox({
+    keyboard: true,
+    arrows: true,
+    zoom: false,
+    buttons: [
+      "close"
+    ],
+    slideClass: "fancybox",
+    clickSlide: "close",
+    clickOutside: "close",
 
-  // $(".kitchen-1").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-1~.item__right-arrow',
-  //   prevArrow: '.kitchen-1~.item__left-arrow'
-  // });
-
-  // $(".kitchen-2").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-2~.item__right-arrow',
-  //   prevArrow: '.kitchen-2~.item__left-arrow'
-  // });
-
-  // $(".kitchen-3").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-3~.item__right-arrow',
-  //   prevArrow: '.kitchen-3~.item__left-arrow'
-  // });
-
-  // $(".kitchen-4").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-4~.item__right-arrow',
-  //   prevArrow: '.kitchen-4~.item__left-arrow'
-  // });
-
-  // $(".kitchen-5").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-5~.item__right-arrow',
-  //   prevArrow: '.kitchen-5~.item__left-arrow'
-  // });
-
-  // $(".kitchen-6").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-6~.item__right-arrow',
-  //   prevArrow: '.kitchen-6~.item__left-arrow'
-  // });
-
-  // $(".kitchen-7").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-7~.item__right-arrow',
-  //   prevArrow: '.kitchen-7~.item__left-arrow'
-  // });
-
-  // $(".kitchen-8").slick({
-  //   infinite: true,
-  //   dots: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   nextArrow: '.kitchen-8~.item__right-arrow',
-  //   prevArrow: '.kitchen-8~.item__left-arrow'
-  // });
+	});
 
 })
